54fa38d2c1cd8315ce25e0a03cb701e15c7541f6
dmod <adarmody@gmail.com>
2019-03-03 11:39:04 -0500

    Initial import for metrics checker

--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
+const request = require('request');
 
+const CURRENT_TRAFFIC_URL = 'http://71.244.212.145:8080/dump1090/data.json';
+
+let all_planes = {};
+
+let metrics_checker = (error, response, body) => {
+    //console.log('error:', error);
+    //console.log('statusCode:', response && response.statusCode);
+    console.log('all_planes size:', Object.keys(all_planes).length);
+
+    let current_planes = JSON.parse(body);
+    current_planes.forEach(x => {
+        if (all_planes[x.hex]) {
+            //console.log('we already know about ' + x.hex);
+        } else {
+            console.log(x.hex + ' is NEW!');
+            all_planes[x.hex] = x;
+        }
+    });
+}
+
+function plane_timer() {
+    request(CURRENT_TRAFFIC_URL, metrics_checker);
+}
+
+setInterval(plane_timer, 10000);
+
+//
+// START EXPRESS
+//
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 // Priority serve any static files.
@@ -17,21 +45,11 @@ app.get('/api/v1/test', async (req, res) => {
 
 app.get('/api/v1/current_traffic', async (req, res) => {
     console.log("someone called current_traffic");
-    http.get('http://71.244.212.145:8080/dump1090/data.json', (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            res.json(JSON.parse(data));
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
+    request(CURRENT_TRAFFIC_URL, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        let json = JSON.parse(body);
+        res.json(json);
     });
 })
 
